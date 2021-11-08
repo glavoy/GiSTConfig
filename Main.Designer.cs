a3a3e31439be60f5eb28c1748f1ea84f076afc92
glavoy <geofflavoy@gmail.com>
2021-11-08 12:50:37 +0300

    Added the ability to create both the Access database and the XML files or create the XML files only

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.ButtonXML = new System.Windows.Forms.Button();
             this.labelVersion = new System.Windows.Forms.Label();
+            this.radioButtonBoth = new System.Windows.Forms.RadioButton();
+            this.radioButtonXMLOnly = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // ButtonXML
@@ -39,7 +41,7 @@
             this.ButtonXML.Name = "ButtonXML";
             this.ButtonXML.Size = new System.Drawing.Size(223, 80);
             this.ButtonXML.TabIndex = 0;
-            this.ButtonXML.Text = "Generate XML file(s)\r\nand\r\nMS Access database";
+            this.ButtonXML.Text = "Generate XML file(s)\r\nand/or\r\nMS Access database";
             this.ButtonXML.UseVisualStyleBackColor = true;
             this.ButtonXML.Click += new System.EventHandler(this.ButtonXML_Click);
             // 
@@ -53,11 +55,33 @@
             this.labelVersion.TabIndex = 1;
             this.labelVersion.Text = "Version: ";
             // 
+            // radioButtonBoth
+            // 
+            this.radioButtonBoth.AutoSize = true;
+            this.radioButtonBoth.Location = new System.Drawing.Point(296, 234);
+            this.radioButtonBoth.Name = "radioButtonBoth";
+            this.radioButtonBoth.Size = new System.Drawing.Size(163, 17);
+            this.radioButtonBoth.TabIndex = 3;
+            this.radioButtonBoth.Text = "Both Database and XML files";
+            this.radioButtonBoth.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonXMLOnly
+            // 
+            this.radioButtonXMLOnly.AutoSize = true;
+            this.radioButtonXMLOnly.Location = new System.Drawing.Point(296, 257);
+            this.radioButtonXMLOnly.Name = "radioButtonXMLOnly";
+            this.radioButtonXMLOnly.Size = new System.Drawing.Size(90, 17);
+            this.radioButtonXMLOnly.TabIndex = 4;
+            this.radioButtonXMLOnly.Text = "XML files only";
+            this.radioButtonXMLOnly.UseVisualStyleBackColor = true;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButtonXMLOnly);
+            this.Controls.Add(this.radioButtonBoth);
             this.Controls.Add(this.labelVersion);
             this.Controls.Add(this.ButtonXML);
             this.Name = "Main";
@@ -72,6 +96,8 @@
 
         private System.Windows.Forms.Button ButtonXML;
         private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.RadioButton radioButtonBoth;
+        private System.Windows.Forms.RadioButton radioButtonXMLOnly;
     }
 }
 
