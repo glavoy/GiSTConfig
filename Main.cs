01af1544596b5d42d1c5b0f34a44fadc8f91eb73
Geoff Lavoy <geofflavoy@gmail.com>
2025-04-09 10:22:34 +0300

    Updated instructions

--- Main.cs
+++ Main.cs
@@ -54,24 +54,24 @@ namespace generatexml
         readonly string excelFile = "C:\\Users\\glavoy\\Box\\SONET R01-UCSF and IDRC\\PILOTS- current 2025\\Pilot 1- Network case finding\\Final instruments\\SONET Data Dictionary NCF Surveys 2025-04-08.xlsx";
 
         //***************************
-        // Path to XML file - this is where the generated xml files wil be written
+        // Path to XML file - this is where the generated xml files will be written
         readonly string xmlPath = "C:\\Users\\glavoy\\Dropbox\\IDRC\\SONET2\\Applications\\sonet_ncf_pilot\\xml\\";
 
         //***************************
-        // Path to log file
+        // Path to log file - keeps track of errors
         readonly string logfilePath = "C:\\temp\\";
 
         //***************************
-        // Path to MS Access database
+        // Path to MS Access database - will be created
         readonly string accessDB = "C:\\SONET\\NCF_Pilot\\MSAccessDatabase\\SONET_NCF_Pilot.mdb";
 
         //***************************
-        // Source database to copy tables
-        //public string sourceDatabasePath = "C:\\SONET\\NCF_Pilot\\MSAccessDatabase\\SONET_NCF_Pilot - Master.mdb";
-
-        //***************************
         // name of the source tables to copy
+        // Create a MS Access databse with the name as the database above, except with " - master" appended to the name
+        // For example, 'SONET_NCF_Pilot.mdb' and 'SONET_NCF_Pilot - Master.mdb'
+        // The software will look for this database to copy the tables from. Below is a list of tables you want to copy from the 'master' to the newly created MS Access databse.
         public string[] sourceTableNames = { "bl_complete", "hhmembers", "households", "sn_complete", "tb_cases", "villages", "fingerprints", "config", "formchanges", "audittrail" };
+
 
 
 
